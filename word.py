2d205c270f245a3c382c36a3ee2f027ea8a7a59c
unknownahmad <sbaia1870@gmail.com>
2026-02-11 18:28:45 +0100

    Implement word frequency analysis function to return top five words

---
+++ word.py
@@ -0,0 +1,31 @@
+def get_count(item):
+   return item[1]
+
+
+def get_top_words(text):
+   clean_text = text.lower()
+   punctuation = '.,!?:;"\'()[]-'
+   for char in punctuation:
+       clean_text = clean_text.replace(char, " ")
+   words = clean_text.split()
+  
+   stop_words = ["the", "and", "is", "of", "to", "a", "in", "it", "that", "or", "for", "on", "are", "this", "my", "be", "as","which","from"]
+  
+   filtered_words = []
+   for w in words:
+       if w not in stop_words:
+           filtered_words.append(w)
+
+
+   counts = {}
+  
+   for w in filtered_words:
+       if w in counts:
+           counts[w] = counts[w] + 1
+       else:
+           counts[w] = 1
+          
+   pairs = list(counts.items())
+  
+   sorted_words = sorted(pairs, key=get_count, reverse=True)
+   return sorted_words[:5]
