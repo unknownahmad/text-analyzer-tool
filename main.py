9d8777895487a386e23e347e2d96a86ad78307da
unknownahmad <sbaia1870@gmail.com>
2026-02-11 18:03:29 +0100

    Add welcome function with instructions for text analysis

---
+++ main.py
@@ -0,0 +1,22 @@
+def welcome():
+    print("=" * 60)
+    print("       📊   T E X T   A N A L Y Z E R   P R O   📊")
+    print("=" * 60)
+    print("         Analyze words, letters, and sentences.")
+    print("-" * 60)
+    print("") 
+
+    print("Instructions:")
+    print("1. Copy your text.")
+    print("2. Paste it below.")
+    print("3. Press ENTER.")
+    print("")
+
+    text_input = input("PASTE YOUR TEXT HERE:\n>> ")
+    return text_input
+
+
+
+
+
+
